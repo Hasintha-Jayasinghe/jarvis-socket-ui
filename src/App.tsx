84c766879bbe44f9f791f1484c96e7dc03b13d96
Hasintha Jayasinghe <hasinthajayasinghe07@gmail.com>
2021-04-03 23:54:54 +0530

    Finished with the basic UI and features

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import io from 'socket.io-client';
+import { __prod__ } from './constants';
 
 function App() {
+  const [name, setName] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+  const socket = io.connect(
+    `ws://${__prod__ ? '54.234.116.155:5000' : 'localhost:5000'}`
+  );
+  const [disabled, setDisabled] = useState<boolean>(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <form className="app__input">
+        <label htmlFor="name">Name:</label>
+        <input
+          value={name}
+          onChange={e => setName(e.target.value)}
+          name="name"
+          type="text"
+        />
+        <label htmlFor="message">Message:</label>
+        <input
+          name="message"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+          type="text"
+        />
+        <button
+          onClick={() => {
+            setDisabled(true);
+            socket.send(
+              JSON.stringify({
+                name,
+                message,
+                id: Math.floor(Math.random() * 5000),
+              })
+            );
+            setTimeout(() => {
+              setDisabled(false);
+            }, 3000);
+          }}
+          disabled={disabled}
         >
-          Learn React
-        </a>
-      </header>
+          Send
+        </button>
+      </form>
     </div>
   );
 }
